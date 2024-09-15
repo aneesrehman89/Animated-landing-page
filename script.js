1c341aa278a9cf0dbbbb7be2b74bf23a81e09e52
aneesrehman <115420485+aneesrehman89@users.noreply.github.com>
2024-09-14 20:06:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+gsap.to('.imgComtainer', {
+    width:"100%",
+    ease: "Expo.easeInOut",
+    duration:2,
+    stagger: 2
+})
+gsap.to('.text h1', {
+    ease: "Expo.easeInOut",
+    stagger:2,
+    top: 0
+} )
+gsap.to('.text h1', {
+    delay: 2,
+    ease: "Expo.easeInOut",
+    stagger:2,
+    top: "-100%"
+} )
